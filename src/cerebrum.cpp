8fdac2b999f3b0364859cb855f1aca59a017119a
vengeance chess <vengeanceChess@gmail.com>
2021-12-12 12:00:44 +0000

    fix memcpy for loading NN file using incbin

--- src/cerebrum.cpp
+++ src/cerebrum.cpp
@@ -235,7 +235,7 @@ int nn_load(NN_Network* nn, char* filename) {
 	fread(st, sizeof(NN_Storage), 1, file);
 	*/
 	
-	memcpy(&st, gNetworkData, sizeof(NN_Storage));
+	memcpy(st, &gNetworkData, sizeof(NN_Storage));
 	
 	for (size_t i = 0; i < (sizeof(st->W0) / sizeof(st->W0[0])); i++) {
 		nn->W0[i] = st->W0[i] / FACTOR;
